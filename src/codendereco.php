03bdc5aad3d5467457dfcd603101192fc525f65b
Jieff Cavalcanti <jieff.cavalcanti@gmail.com>
2022-07-27 17:32:52 -0300

    trata o codigo de endereço do cliente

--- src/codendereco.php
+++ src/codendereco.php
@@ -19,25 +19,18 @@
         while(($data = fgetcsv($object, 1000, ",")) !== FALSE)
         {
             
-            $codcliente = mb_convert_encoding($data[2], "utf-8");
-            $ativa      = mb_convert_encoding($data[3], "utf-8");
+            $cli_clientes_codcliente    = mb_convert_encoding($data[1], "utf-8"); // pega o id do cliente
+            $cli_enderecos_codendereco  = mb_convert_encoding($data[0], "utf-8"); // pega o codendereco
 
-            if($ativa == "true"){
-                $ativa = 0;
-            } else {
-                $ativa = 1;
-            }
+            $result = $mysqli->query("UPDATE fin_cobrancascliente SET cli_enderecos_codendereco='$cli_enderecos_codendereco' WHERE cli_clientes_codcliente='$cli_clientes_codcliente'");
 
-            $result = $mysqli->query("UPDATE cli_clientes SET situacao=$ativa WHERE codcliente='$codcliente'");
-                                    
         }
  
-
         if($result ){
             echo "Dados inseridos com sucesso";
         } else {
             echo "Error ao inserir os dados". die($mysqli->error);
         }
 
-
+    
     }
