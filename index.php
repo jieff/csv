be794972538313fe381a0ae92f94e9537a9d4f6d
Jieff Cavalcanti <jieff.cavalcanti@hotmail.com>
2023-03-15 21:21:39 -0300

    update

--- index.php
+++ index.php
@@ -7,20 +7,7 @@ require './src/includes/navbar.php';
 //require './src/includes/tables/upload.php'; 
 
 require './src/includes/header.php';
-<<<<<<< HEAD
-require './src/includes/tables/upload.php'; 
-<<<<<<< HEAD
-<<<<<<< HEAD
-require './src/includes/tables/step.php'; 
-=======
-//require './src/includes/tables/step.php'; 
->>>>>>> 9150f47 (primeiro commit)
-=======
-require './src/includes/tables/step.php'; 
->>>>>>> 0578105 (update brdrive)
-=======
 
-//require './src/includes/tables/step.php'; 
->>>>>>> 3436ae5 (update csv brSim)
 
-require './src/includes/footer.html'; 
+require './src/includes/footer.html';
+
--- src/database/conexao.php
+++ src/database/conexao.php
@@ -1,15 +1,15 @@
 <?php
 
-//require_once __DIR__ . '../../../vendor/autoload.php';
-//
-//$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../../');
-//$dotenv->load();
+require_once __DIR__ . '../../../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
 
 
-$host     =  "db01.ksys.net.br";                //$_ENV['DB_HOST'];
-$database =  "bemtevi_brdrive";                //$_ENV['DB_DATABASE'];
-$user     =  "brdrive";                 //$_ENV['DB_USER'];
-$pass     =  "OLFVHRWI";              //$_ENV['DB_PASS'];
+$host     =  $_ENV['DB_HOST'];
+$database =  $_ENV['DB_DATABASE'];
+$user     =  $_ENV['DB_USER'];
+$pass     =  $_ENV['DB_PASS'];
 
 $mysqli = new mysqli($host, $user, $pass, $database);
 mysqli_set_charset($mysqli, "utf8");
